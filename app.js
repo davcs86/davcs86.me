b641714e90c8c1e9ef2dae78b338661be301ea0e
David Castillo <dcastillo@lyft.com>
2020-04-24 01:47:29 -0700

    revert server info

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const express = require('express');
-const si  = require('systeminformation');
 
 const app = express();
 const port = 3000;
-
-app.get('/server-info', async(req, res) => {
-    try {
-        const osInfo = await si.osInfo();
-        res.send(`Hello world! (OS info: ${osInfo}`);
-    } catch (e) {
-        console.log(e);
-    };
-});
 
 app.get('/', (req, res) => res.send('Hello world (expressjs)!'));
 
