229571e8abb2a3a13f4b44481a1f5601fff0833f
David Castillo <davcs86@gmail.com>
2020-04-24 01:35:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const si  = require('systeminformation');
 const app = express();
 const port = 3000;
 
-app.get('/', async(req, res) => {
+app.get('/server-info', async(req, res) => {
     try {
         const osInfo = await si.osInfo();
         res.send(`Hello world! (OS info: ${osInfo}`);
     } catch (e) {
-        console.log(e)
+        console.log(e);
     };
 });
+
+app.get('/', (req, res) => res.send('Hello world (expressjs)!'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
